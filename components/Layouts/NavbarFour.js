0a256eaaf4cd95236345a7288fe23ba2bd11b18c
Seon <sunju701@gmail.com>
2022-11-01 16:11:25 +0900

    login, design update

--- components/Layouts/NavbarFour.js
+++ components/Layouts/NavbarFour.js
@@ -6,7 +6,7 @@ export default function NavbarFour() {
     // Search Form
     const [searchForm, setSearchForm] = useState(false);
     const isSignIn = useRef(false);
-    const [state, setState] = useState({display: false, collapsed: true});
+    const [state, setState] = useState({ display: false, collapsed: true });
     const [_isMounted, setIsMounted] = useState(false);
     const [cookies, setCookie] = useCookies(['id']);
 
@@ -43,57 +43,57 @@ export default function NavbarFour() {
 
     }, []);
 
-    
+
     const { collapsed } = state;
     const classOne = collapsed ? 'collapse navbar-collapse' : 'collapse navbar-collapse show';
     const classTwo = collapsed ? 'navbar-toggler navbar-toggler-right collapsed' : 'navbar-toggler navbar-toggler-right';
-    
+
     return (
-            <>
-               <div id="navbar" className="navbar-area navbar-style-four">
-                    <div className="main-nav">
-                        <div className="container-fluid">
-                            <nav className="navbar navbar-expand-md navbar-light">
-                                <Link href="/">
-                                    <a className="navbar-brand">
-                                        <img src="/images/rvsm_logo2.png" className="optional-logo" alt="logo" />
-                                    </a>
-                                </Link>
+        <>
+            <div id="navbar" className="navbar-area navbar-style-four">
+                <div className="main-nav">
+                    <div className="container-fluid">
+                        <nav className="navbar navbar-expand-md navbar-light">
+                            <Link href="/">
+                                <a className="navbar-brand">
+                                    <img src="/images/rvsm_logo2.png" className="optional-logo" alt="logo" />
+                                </a>
+                            </Link>
 
-                                <button 
-                                    onClick={toggleNavbar} 
-                                    className={classTwo}
-                                    type="button" 
-                                    data-toggle="collapse" 
-                                    data-target="#navbarSupportedContent" 
-                                    aria-controls="navbarSupportedContent" 
-                                    aria-expanded="false" 
-                                    aria-label="Toggle navigation"
-                                >
-                                    <span className="icon-bar top-bar"></span>
-                                    <span className="icon-bar middle-bar"></span>
-                                    <span className="icon-bar bottom-bar"></span>
-                                </button>
+                            <button
+                                onClick={toggleNavbar}
+                                className={classTwo}
+                                type="button"
+                                data-toggle="collapse"
+                                data-target="#navbarSupportedContent"
+                                aria-controls="navbarSupportedContent"
+                                aria-expanded="false"
+                                aria-label="Toggle navigation"
+                            >
+                                <span className="icon-bar top-bar"></span>
+                                <span className="icon-bar middle-bar"></span>
+                                <span className="icon-bar bottom-bar"></span>
+                            </button>
 
-                                <div className={classOne} id="navbarSupportedContent">
-                                    
-                                    <ul className="navbar-nav">
-                                        <li className="nav-item">
-                                            <Link href="/" activeClassName="active">
-                                                <a className="nav-link">
-                                                    Home
-                                                </a>
-                                            </Link>
-                                        </li>
+                            <div className={classOne} id="navbarSupportedContent">
 
-                                        <li className="nav-item">
-                                            <Link href="/services" activeClassName="active">
-                                                <a className="nav-link">Services</a>
-                                            </Link>
-                                        </li>
- 
-                                        {isSignIn.current === false
-                                        ? 
+                                <ul className="navbar-nav">
+                                    <li className="nav-item">
+                                        <Link href="/" activeClassName="active">
+                                            <a className="nav-link">
+                                                Home
+                                            </a>
+                                        </Link>
+                                    </li>
+
+                                    <li className="nav-item">
+                                        <Link href="/services" activeClassName="active">
+                                            <a className="nav-link">Services</a>
+                                        </Link>
+                                    </li>
+
+                                    {isSignIn.current === false
+                                        ?
                                         <li className="nav-item">
                                             <Link href="/login" activeClassName="active">
                                                 <a className="nav-link">Log In</a>
@@ -101,60 +101,60 @@ export default function NavbarFour() {
                                         </li>
                                         :
                                         <>
-                                        <li className="nav-item">
-                                            <Link href="/mypage" activeClassName="active">
-                                                <a className="nav-link">{cookies.id}님</a>
-                                            </Link>
-                                        </li>
-                                        <li className="nav-item">
-                                            <Link href="/logout" activeClassName="active">
-                                                <a className="nav-link">Log Out</a>
-                                            </Link>
-                                        </li>
+                                            <li className="nav-item">
+                                                <Link href="/mypage" activeClassName="active">
+                                                    <a className="nav-link">{cookies.id}님</a>
+                                                </Link>
+                                            </li>
+                                            <li className="nav-item">
+                                                <Link href="/logout" activeClassName="active">
+                                                    <a className="nav-link">Log Out</a>
+                                                </Link>
+                                            </li>
                                         </>
-                                        }
+                                    }
 
-                                    </ul>
-                                    
-                                    <div className="others-options">
+                                </ul>
 
-                                        <div className="option-item">
-                                            <i 
-                                                onClick={handleSearchForm} 
-                                                className="search-btn flaticon-search"
-                                                style={{
-                                                    display: state.searchForm ? 'none' : 'block'
-                                                }}
-                                            ></i>
+                                <div className="others-options">
 
-                                            <i 
-                                                onClick={handleSearchForm} 
-                                                className={`close-btn fas fa-times ${state.searchForm ? 'active' : ''}`}
-                                            ></i>
-                                            
-                                            <div 
-                                                className="search-overlay search-popup"
-                                                style={{
-                                                    display: state.searchForm ? 'block' : 'none'
-                                                }}
-                                            >
-                                                <div className='search-box'>
-                                                    <form className="search-form">
-                                                        <input className="search-input" name="search" placeholder="Search" type="text" />
-                                                        <button className="search-button" type="submit">
-                                                            <i className="fas fa-search"></i>
-                                                        </button>
-                                                    </form>
-                                                </div>
+                                    <div className="option-item">
+                                        <i
+                                            onClick={handleSearchForm}
+                                            className="search-btn flaticon-search"
+                                            style={{
+                                                display: state.searchForm ? 'none' : 'block'
+                                            }}
+                                        ></i>
+
+                                        <i
+                                            onClick={handleSearchForm}
+                                            className={`close-btn fas fa-times ${state.searchForm ? 'active' : ''}`}
+                                        ></i>
+
+                                        <div
+                                            className="search-overlay search-popup"
+                                            style={{
+                                                display: state.searchForm ? 'block' : 'none'
+                                            }}
+                                        >
+                                            <div className='search-box'>
+                                                <form className="search-form">
+                                                    <input className="search-input" name="search" placeholder="Search" type="text" />
+                                                    <button className="search-button" type="submit">
+                                                        <i className="fas fa-search"></i>
+                                                    </button>
+                                                </form>
                                             </div>
                                         </div>
-                                                
                                     </div>
+
                                 </div>
-                            </nav>
-                        </div>
+                            </div>
+                        </nav>
                     </div>
                 </div>
-            </>
-        );
-    }
+            </div>
+        </>
+    );
+}
--- pages/_document.js
+++ pages/_document.js
@@ -1,7 +1,7 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document';
 
 class MyDocument extends Document {
-    
+
     static async getInitialProps(ctx) {
         const initialProps = await Document.getInitialProps(ctx)
         return { ...initialProps }
@@ -19,7 +19,7 @@ class MyDocument extends Document {
                         href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i,800,800i&display=swap"
                         rel="stylesheet"
                     />
-                    <link rel="icon" type="image/png" href="/favicon.png"></link>
+                    <link rel="icon" type="image/png" href="/images/rvsm_no_logo.png"></link>
                 </Head>
                 <body>
                     <Main />
