a8a288cd308cd8568016bb4412a5367ea61b29a4
Seon <sunju701@gmail.com>
2022-11-03 22:20:20 +0900

    patientRegistration update

--- pages/rvsm/registration.js
+++ pages/rvsm/registration.js
@@ -2,11 +2,13 @@ import { useEffect, useState } from "react";
 import NavbarFour from "../../components/Layouts/NavbarFour";
 import Footer from "../../components/Layouts/Footer";
 import style from "../../styles/registration.module.css"
-import {patientRegistrationPoint} from "../../toServer/API-AccessPoint";
+import { patientRegistrationPoint } from "../../toServer/API-AccessPoint";
 
 export default function registration() {
-    
+
     const [hospitalCode, setHospitalCode] = useState("");
+    const [message, setMessage] = useState("병원코드, 환자 id, 환자 이름을 입력해주세요.");
+    const [messageColor, setMessageColor] = useState("black");
     const [id, setId] = useState("");
     const [patientName, setPatientName] = useState("");
 
@@ -20,11 +22,11 @@ export default function registration() {
     const patientNameChange = (e) => {
         setPatientName(e.target.value);
     }
-    const sendPost=async()=>{
+    const sendPost = async () => {
         const response = await fetch(patientRegistrationPoint, {
             method: "POST",
             body: JSON.stringify({
-                hospitalCode: hospitalCode,
+                hospitalCode: hospitalCode.toUpperCase(),
                 id: id,
                 patientName: patientName
             }),
@@ -32,26 +34,26 @@ export default function registration() {
                 "Content-Type": "application/json",
             }
         });
-        const data=await response.json();
-        let status=data.status;
-        if(status==200){
-            alert("등록이 완료되었습니다.");
+        const data = await response.json();
+        let status = data.status;
+        if (status == 200) {
+            alert(data.name + "님의 등록이 완료되었습니다.");
         }
-        else if(status==400){
-            alert("등록 실패. 아이디 중복입니다.");
-        }
-        else if(status==404){
-            alert("등록 실패. 병원코드를 확인하세요.");
+        else if (status == 400) {
+            setMessage("등록 실패. 입력 정보를 확인하세요.");
+            setMessageColor("red");
         }
     }
-   
+
     return (
         <div className={style.wrap}>
             <NavbarFour />
             <div className={style.registration_box}>
-                <input className={style.input_box} type="text" value={hospitalCode} onChange={hospitalCodeChange} placeholder="병원 코드 입력"></input>
-                <input className={style.input_box} type="text" value={id} onChange={idChange} placeholder="환자id입력"></input>
+                <input className={style.input_box} type="text" value={hospitalCode} onChange={hospitalCodeChange} placeholder="병원코드 입력"></input>
+                <input className={style.input_box} type="text" value={id} onChange={idChange} placeholder="환자 ID 입력"></input>
                 <input className={style.input_box} type="text" value={patientName} onChange={patientNameChange} placeholder="환자 이름 입력"></input>
+                <span style={{ color: messageColor }}>{message}</span>
+                <br></br>
                 <button className={style.registration_btn} onClick={sendPost}>환자 등록</button>
             </div>
             <Footer />
--- toServer/API-AccessPoint.js
+++ toServer/API-AccessPoint.js
@@ -6,5 +6,5 @@
 // Development and testing
 export const socketPoint = `http://localhost:3333`;
 export const loginPoint = `http://localhost:3333/login/session`;
-export const patientListPoint=`http://localhost:3333/patientList/session`;
-export const patientRegistrationPoint=`http://localhost:3333/patientRegistration/session`
+export const patientListPoint = `http://localhost:3333/patientList/session`;
+export const patientRegistrationPoint = `http://localhost:3333/patientRegistration/session`
